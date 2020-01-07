04bdcd7b1391d9775c60ae2e3e0d0f175579d028
Philip Cutler <greenthegarden@gmail.com>
2020-01-07 03:31:35 +0000

    test for no args

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,6 +3,10 @@
 import enviroplusmonitor.app as app
 
 
-def test_app_parse_args():
+def test_app_parse_args_config():
     args = app.parse_args(['-c', 'test'])
     assert args.config == 'test'
+
+def test_app_parse_args_none():
+    args = app.parse_args([])
+    assert args.config == app.default_configuration_file
