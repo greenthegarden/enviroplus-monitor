6dc70fc0ddc30a7daa9c4b9cc35236aea57dec76
Philip Cutler <cutlervufamily@gmail.com>
2020-04-18 19:52:07 +0930

    add support for pydantic

--- enviroplusmonitor/sensors/dht22.py
+++ enviroplusmonitor/sensors/dht22.py
@@ -13,6 +13,10 @@ import enviroplusmonitor.utilities.configurationhandler as configurationhandler
 import enviroplusmonitor.utilities.mqttclienthandler as mqttclienthandler
 import enviroplusmonitor.utilities.unitregistryhandler as unitregistryhandler
 
+from jsonschema import validate
+from typing import List
+from pydantic import BaseModel, ValidationError
+
 module_logger = logging.getLogger(
     configurationhandler.config["logging"]["MODULE_LOGGER"]
 )
@@ -20,6 +24,25 @@ module_logger = logging.getLogger(
 # Initial the dht device, with data pin connected to:
 dhtDevice = adafruit_dht.DHT22(board.D4)
 
+class ConfigPayload(BaseModel):
+  device_class: str
+  name: str
+  state_topic: str
+  unit_of_measurement: str
+  value_template: str
+
+class Measurement(BaseModel):
+  label: str
+  value: float
+  units: str
+
+class Dht22Measurement(BaseModel):
+  sensor: str = (configurationhandler.config["sensors"]["DHT22_LABEL"])
+  measurements: List[Measurement]
+
+class Dht22MeasurementPayload(BaseModel):
+  temperature: float
+  humidity: float
 
 def sensor_readings():
     try:
@@ -39,22 +62,26 @@ def sensor_readings():
 def measurement():
     readings = sensor_readings()
     if readings is not None:
-        data = {
-            "sensor": str(configurationhandler.config["sensors"]["DHT22_LABEL"]),
-            "measurements": {
-                "temperature": {
-                    "value": readings.get("temperature").magnitude,
-                    "units": readings.get("temperature").units,
-                },
-                "humidity": {
-                    "value": readings.get("humidity_relative").magnitude,
-                    "units": readings.get("humidity_relative").units,
-                },
-            },
-        }
-        return data
+      try:
+        data = Dht22Measurement(
+          measurements = [
+            Measurement(
+              label = "temperature",
+              value = readings.get("temperature").magnitude,
+              units = readings.get("temperature").units,
+            ),
+            Measurement(
+              label = "humidity",
+              value = readings.get("humidity").magnitude,
+              units = readings.get("humidity").units,
+            )
+          ]
+        )
+        return data.dict()
+      except ValidationError as e:
+        module_logger.error(e.json())
     else:
-        return None
+      return None
 
 
 # payloads for dynamic mqtt support for home assistant
@@ -78,14 +105,14 @@ CONFIG_TOPIC_TEMP = str(
 STATE_TOPIC = "homeassistant/sensor/enviroplus/" + str(configurationhandler.config["enviroplus"]["id"]) + "/" +  str(configurationhandler.config["sensors"]["DHT22_LABEL"]) + "/" + "state"
 
 # Configuration payload no1: {"device_class": "temperature", "name": "Temperature", "state_topic": "homeassistant/sensor/sensorBedroom/state", "unit_of_measurement": "°C", "value_template": "{{ value_json.temperature}}" }
-CONFIG_PAYLOAD_TEMP_DICT = {
-    "device_class": "temperature",
-    "name": "Temperature",
-    "state_topic": STATE_TOPIC,
-    "unit_of_measurement": "°C",
-    "value_template": "{{ value_json.temperature}}"
-}
-CONFIG_PAYLOAD_TEMP_JSON = json.dumps(CONFIG_PAYLOAD_TEMP_DICT)
+config_payload_temp = ConfigPayload(
+  device_class = 'temperature',
+  name = "Temperature",
+  state_topic = STATE_TOPIC,
+  unit_of_measurement = "°C",
+  value_template = "{{ value_json.temperature}}"
+)
+config_payload_temp_json = json.dumps(config_payload_temp.dict())
 
 CONFIG_TOPIC_HUM = str(
     "homeassistant/sensor"
@@ -102,14 +129,14 @@ CONFIG_TOPIC_HUM = str(
 )
 
 # Configuration payload no1: {"device_class": "temperature", "name": "Temperature", "state_topic": "homeassistant/sensor/sensorBedroom/state", "unit_of_measurement": "°C", "value_template": "{{ value_json.temperature}}" }
-CONFIG_PAYLOAD_HUM_DICT = {
-    "device_class": "humidity",
-    "name": "Humidity",
-    "state_topic": STATE_TOPIC,
-    "unit_of_measurement": "%",
-    "value_template": "{{ value_json.humidity}}"
-}
-CONFIG_PAYLOAD_HUM_JSON = json.dumps(CONFIG_PAYLOAD_HUM_DICT)
+config_payload_hum = ConfigPayload(
+  device_class = "humidity",
+  name = "Humidity",
+  state_topic = STATE_TOPIC,
+  unit_of_measurement = "%",
+  value_template = "{{ value_json.humidity}}"
+)
+config_payload_hum_json = json.dumps(config_payload_hum.dict())
 
 # Common state payload: { "temperature": 23.20, "humidity": 43.70 }
 
@@ -124,8 +151,8 @@ module_logger.info("Topic str: {topic}".format(topic=TOPIC_STR))
 
 def publish_configuration_topics():
     # module_logger.info("Payload: {payload}".format(payload=payload))
-    mqttclienthandler.client.publish(CONFIG_TOPIC_TEMP, CONFIG_PAYLOAD_TEMP_JSON)
-    mqttclienthandler.client.publish(CONFIG_TOPIC_HUM, CONFIG_PAYLOAD_HUM_JSON)
+    mqttclienthandler.client.publish(CONFIG_TOPIC_TEMP, config_payload_temp_json)
+    mqttclienthandler.client.publish(CONFIG_TOPIC_HUM, config_payload_hum_json)
 
 # weather,location=us-midwest,season=summer temperature=82
 def publish_influx_payload():
@@ -156,10 +183,9 @@ def publish_mqtt_discoverable_payload():
     data = measurement()
     if data is not None:
         measurements = data.get("measurements")
-        payload_dict = {
-            "temperature": round((measurements.get("temperature")).get("value"), 2),
-            "humidity": round((measurements.get("humidity")).get("value"), 2)
-        }
-        payload = json.dumps(payload_dict)
-        module_logger.info("Payload: {payload}".format(payload=payload))
+        payload = Dht22MeasurementPayload(
+            temperature = round((measurements.get("temperature")).get("value"), 2),
+            humidity = round((measurements.get("humidity")).get("value"), 2)
+        )
+        module_logger.info("Payload: {payload}".format(payload=json.dump(payload.dict())))
         mqttclienthandler.client.publish(STATE_TOPIC, payload)
