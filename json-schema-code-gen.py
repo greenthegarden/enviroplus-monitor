b0b095922198902ceb0e204836f75c30f1e57b27
Philip Cutler <cutlervufamily@gmail.com>
2020-04-23 09:35:25 +0930

    attempt run as python example

--- json-schema-code-gen.py
+++ json-schema-code-gen.py
@@ -1,9 +1,11 @@
+#!/usr/bin/env python3
+
 from jsonschemacodegen import python as pygen
 import yaml
 
 filename = 'asyncapi.yaml'
 
 with open(filename) as fp:
-    schema = yaml.load(fp, Loader=FullLoader)
+    schema = yaml.load(fp, Loader=yaml.FullLoader)
     generator = pygen.GeneratorFromSchema('output_dir')
     generator.Generate(schema=schema, root=None, class_name='Example', filename_base='example')
